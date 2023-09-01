7b4ba25456a1436183094899db7d8d6a123a1d34
lzvckaya <lzvckaya@gmail.com>
2023-09-01 22:32:20 +0900

    add detail page

--- src/App.js
+++ src/App.js
@@ -2,30 +2,49 @@ import "./App.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import data from "./data.js";
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./detail";
 
 function App() {
   let [shoes] = useState(data);
 
   return (
-    <div>
+    <div className="App">
       <Navbar bg="white" variant="white">
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Brand href="/">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} idx={i + 1}></Card>;
-          })}
-        </Row>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} idx={i + 1}></Card>;
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route
+          path="/detail"
+          element={
+            <>
+              <Detail></Detail>
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
